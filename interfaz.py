0020fec04287e837b03a0ab1a9f26ff9254e3c05
Felipe <fel.afta@gmail.com>
2023-08-30 10:50:29 -0400

    se crea funcion para mostrar la descripcion de erores

--- interfaz.py
+++ interfaz.py
@@ -1,11 +1,24 @@
 import tkinter
 from tkinter import ttk
+import json
+
+with open('errores.json', 'r') as archivo:
+    datos=json.load(archivo)
+
+def obtener_desc():
+    dato_ingresado = entrada_texto.get()
+    for error in datos['errores']:
+        if error['codigo'] == dato_ingresado:
+            desc_error.config(text=error['desc'])
+            break
+
+
 #ventana creada
 ventana = tkinter.Tk()
 
 #titulo ventana
 ventana.title("Interfaz de Codigo")
-titulo = ttk.Label(ventana, text= "Ingrese el codigo:  ",font= "calibri 20 bold" ) 
+titulo = ttk.Label(ventana, text= "Ingrese El Codigo:  ",font= "calibri 20 bold" ) 
 titulo.pack(pady=10, padx= 10)
 
 #entrada de txt
@@ -13,7 +26,7 @@ entrada_texto= ttk.Entry(ventana)
 entrada_texto.pack(padx=10 , pady= 10  )
 
 #boton Tk
-btn_buscar= ttk.Button(ventana, text= "buscar codigo")
+btn_buscar= ttk.Button(ventana, text= "buscar codigo", command= obtener_desc)
 btn_buscar.pack(pady=10 ,padx=10)
 
 #desc de busqueda
